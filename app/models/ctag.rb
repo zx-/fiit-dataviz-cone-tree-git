e2afde546ac81677b59ed2812cc5613ddab4d015
zx- <robertcuprik@hotmail.com>
2016-04-11 22:30:46 +0200

    Adds ctag parser and tree creation to repo

---
+++ app/models/ctag.rb
@@ -0,0 +1,28 @@
+require "csv"
+
+class Ctag
+  def initialize(file)
+    @tags = {}
+
+    tags = File.readlines(file)
+    tags.drop(6).each do |tag|
+      attrs = tag.split("\t")
+
+      file_path = attrs[1]
+      @tags[file_path] = [] if @tags[file_path].nil?
+
+      @tags[file_path] << {
+          name: attrs[0],
+          def: attrs[2],
+          type: attrs[3],
+          line: attrs[4].split(':')[1],
+          language: attrs[5].split(':')[1].strip,
+          info: attrs[6]&.strip
+      }
+    end
+  end
+
+  def for_file(file_path)
+    @tags[file_path]
+  end
+end
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -16,10 +16,41 @@ class Repo < ActiveRecord::Base
     ## generate ctags
     val = system("cd #{Rails.root}/repos/#{repo.id} && ctags -R -u --fields=+a+K+l+m+n+S+t")
 
-    ## generate fileList
-    files = system('find . -printf "%p\n" | sed "s/^\.\///"')
-    puts files
+    tags = Ctag.new "#{Rails.root}/repos/#{repo.id}/tags"
 
+
+    ## generate tree
+    tree = { files: [] }
+
+    system("cd #{Rails.root}/repos/#{repo.id} "+'&& find . -printf "%p:%y\n" | sed "s/^\.\///" > ___fileList')
+    File.open("#{Rails.root}/repos/#{repo.id}/___fileList").each_line do |file|
+      ft = file.split(':')
+      file_name = ft[0].strip
+      file_type = ft[1].strip
+      file_tags = tags.for_file(file_name)
+
+
+      parent_node = tree
+      file_tree = file_name.split('/')
+      path = file_tree[0..-2]
+
+      # traverse existing tree
+      path.each { |dir| parent_node = parent_node[dir] }
+
+      if file_type == 'd'
+        parent_node[file_tree.last] = { files: [] }
+      else
+        parent_node[:files] << {
+            name: file_tree.last,
+            tags: file_tags
+        }
+      end
+
+    end
+    repo.tree = tree
+    repo.save
+
+    puts tree
 
     return repo
   end
